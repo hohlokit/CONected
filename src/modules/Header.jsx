fc1753c45aac1b7be79b1342ae5f39414ed228c4
hohlokit <kolik2001@gmail.com>
2024-06-01 01:02:18 +0300

    fix

--- src/modules/Header.jsx
+++ src/modules/Header.jsx
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "../components";
 
 const Header = () => {
+  const user = JSON.parse(localStorage.getItem("user"));
+  const [avatarSrc, setAvatarSrc] = useState(user.avatar || "");
+
+  const name =
+    user.type === "company"
+      ? user.company
+      : `${user.firstname} ${user.lastname}`;
+
+  const onError = () => {
+    setAvatarSrc(
+      "https://res.cloudinary.com/dxdrvm3gs/image/upload/v1717090361/myqc8dkk0xttrwwkvkod.png"
+    );
+  };
+
   return (
     <div className="fixed top-0 px-12 py-8 shadow-md w-full bg-body bg-no-repeat bg-cover flex flex-row justify-between">
       <Link to="/vacancies">Вакансії</Link>
-      <Link className={"capitalize"} to="/profile">
-        Профіль
-      </Link>
+      <div className="flex flex-row items-center gap-2">
+        <img
+          onError={onError}
+          className="rounded-full border border-steam-text-primary w-8 h-8 object-contain"
+          alt="avatar"
+          src={avatarSrc}
+        />
+        <Link className={"capitalize"} to="/profile">
+          {name}
+        </Link>
+      </div>
     </div>
   );
 };
